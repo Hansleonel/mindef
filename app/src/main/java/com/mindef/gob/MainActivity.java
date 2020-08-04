31aeab27dc20567eddd0ff7bac4126f7cd1f719f
hans <hansleonel@gmail.com>
2020-08-03 21:23:03 -0500

    Create RegisterActivity add Date Picker add ToolBar add Icons add Material Design Personalization

--- app/src/main/java/com/mindef/gob/MainActivity.java
+++ app/src/main/java/com/mindef/gob/MainActivity.java
@@ -2,7 +2,9 @@ package com.mindef.gob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        findViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
